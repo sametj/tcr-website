03490514bb6a0e9dff2136466dafcbf698c5c217
Moses <mosesjames2000@gmail.com>
2024-06-01 08:24:05 -0400

    Completed Page Layout

---
+++ src/pages/Stats/Contents.jsx
@@ -0,0 +1,128 @@
+export const BUTTONS = [
+	{
+		buttonText: "Season Standing",
+		subButtons: [
+			{
+				buttonText: "League Wide",
+				content: "nush20",
+			},
+			{
+				buttonText: "Silver Class",
+				content: "nush30",
+			},
+			{
+				buttonText: "Golden Class",
+				content: "nush40",
+			},
+			{
+				buttonText: "Platinum Class",
+				content: "nush50",
+			},
+		],
+	},
+	{
+		buttonText: "Driver Stats",
+		subButtons: [
+			{
+				buttonText: "League Wide",
+				content: "test20",
+			},
+			{
+				buttonText: "Silver Class",
+				content: "test30",
+			},
+			{
+				buttonText: "Golden Class",
+				content: "test40",
+			},
+			{
+				buttonText: "Platinum Class",
+				content: "test50",
+			},
+		],
+	},
+	{
+		buttonText: "Driver Champions",
+		subButtons: [
+			{
+				buttonText: "League Wide",
+				content: "test20",
+			},
+			{
+				buttonText: "Silver Class",
+				content: "test30",
+			},
+			{
+				buttonText: "Golden Class",
+				content: "test40",
+			},
+			{
+				buttonText: "Platinum Class",
+				content: "test50",
+			},
+		],
+	},
+	{
+		buttonText: "Constructors Championship",
+		subButtons: [
+			{
+				buttonText: "League Wide",
+				content: "test20",
+			},
+			{
+				buttonText: "Silver Class",
+				content: "test30",
+			},
+			{
+				buttonText: "Golden Class",
+				content: "test40",
+			},
+			{
+				buttonText: "Platinum Class",
+				content: "test50",
+			},
+		],
+	},
+	{
+		buttonText: "Pole Lap Records",
+		subButtons: [
+			{
+				buttonText: "League Wide",
+				content: "test20",
+			},
+			{
+				buttonText: "Silver Class",
+				content: "test30",
+			},
+			{
+				buttonText: "Golden Class",
+				content: "test40",
+			},
+			{
+				buttonText: "Platinum Class",
+				content: "test50",
+			},
+		],
+	},
+	{
+		buttonText: "Fastest Lap Records",
+		subButtons: [
+			{
+				buttonText: "League Wide",
+				content: "test20",
+			},
+			{
+				buttonText: "Silver Class",
+				content: "test30",
+			},
+			{
+				buttonText: "Golden Class",
+				content: "test40",
+			},
+			{
+				buttonText: "Platinum Class",
+				content: "test50",
+			},
+		],
+	},
+];
